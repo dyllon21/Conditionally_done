973018109b4deb2f38f6977ecd07e8cf63ea0920
dyllon21 <isaacsdyllon111@gmail.com>
2016-11-17 11:20:35 -0500

    used console.log to execute the the coditions

--- if_tuesday.js
+++ if_tuesday.js
@@ -1,8 +1,7 @@
 var day = 'Monday';
 var isTuesday = day === 'Tuesday';
-if (isTuesday){
-  console.log('Hey, hey it is Tuesday!');
-}
-else{
-  console.log('Wednesday : ' + day);
+if (isTuesday) {
+    console.log('Hey, hey it is Tuesday!');
+} else {
+    console.log('Wednesday : ' + day);
 }
