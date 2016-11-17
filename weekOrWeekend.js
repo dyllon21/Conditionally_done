ca048b10e2e1982ab73d203d893a7dfd69173dac
dyllon21 <isaacsdyllon111@gmail.com>
2016-11-17 10:28:23 -0500

    added initialised commit

--- weekOrWeekend.js
+++ weekOrWeekend.js
@@ -1,16 +1,11 @@
 const assert = require('assert');
 var weekOrWeekend = function(day) {
-    if (day.startsWith('S')){
-        //console.log("is a week");
-    return 'weekend'
-}
- else {
-    // console.log('week');
-    return 'week'
-}
 
+    if (!day.startsWith('S')) {
+        return 'week'
+    } else {
+        return 'weekend'
+    }
 };
-weekOrWeekend('Tuesday');
-
-assert.equal(weekOrWeekend('Tuesday'), 'week');
+assert.equal(weekOrWeekend('Wednesday'), 'week');
 assert.equal(weekOrWeekend('Sunday'), 'weekend');
